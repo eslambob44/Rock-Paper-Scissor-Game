3d607892736a424f86c7b00d7306d11baa966d96
eslambob44 <eyasser237@gmail.com>
2024-07-27 19:39:38 +0300

    Finished Rock Paper Scissor Game

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmRockPaperScissorGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRockPaperScissorGame));
+            this.lblGameName = new System.Windows.Forms.Label();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnScissor = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pbComputerChoice = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pbPlayerChoice = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbComputerChoice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayerChoice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGameName
+            // 
+            this.lblGameName.AutoSize = true;
+            this.lblGameName.Font = new System.Drawing.Font("Pristina", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblGameName.Location = new System.Drawing.Point(340, 24);
+            this.lblGameName.Name = "lblGameName";
+            this.lblGameName.Size = new System.Drawing.Size(252, 46);
+            this.lblGameName.TabIndex = 0;
+            this.lblGameName.Text = "Rock Paper Scissor";
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.Location = new System.Drawing.Point(33, 379);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(67, 19);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Tag = "Rock";
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = false;
+            this.btnRock.Click += new System.EventHandler(this.btnGame_Click);
+            // 
+            // btnScissor
+            // 
+            this.btnScissor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnScissor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScissor.Location = new System.Drawing.Point(263, 379);
+            this.btnScissor.Name = "btnScissor";
+            this.btnScissor.Size = new System.Drawing.Size(67, 19);
+            this.btnScissor.TabIndex = 2;
+            this.btnScissor.Tag = "Scissor";
+            this.btnScissor.Text = "Scissor";
+            this.btnScissor.UseVisualStyleBackColor = false;
+            this.btnScissor.Click += new System.EventHandler(this.btnGame_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.Location = new System.Drawing.Point(148, 379);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(67, 19);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Tag = "Paper";
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = false;
+            this.btnPaper.Click += new System.EventHandler(this.btnGame_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.pbPlayerChoice);
+            this.groupBox1.Controls.Add(this.btnPaper);
+            this.groupBox1.Controls.Add(this.btnScissor);
+            this.groupBox1.Controls.Add(this.btnRock);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Pristina", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(21, 120);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 420);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Player";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Pristina", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(438, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 46);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "VS";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.pbComputerChoice);
+            this.groupBox2.Controls.Add(this.pictureBox6);
+            this.groupBox2.Controls.Add(this.pictureBox7);
+            this.groupBox2.Controls.Add(this.pictureBox8);
+            this.groupBox2.Font = new System.Drawing.Font("Pristina", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.groupBox2.Location = new System.Drawing.Point(544, 120);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(374, 420);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Computer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 382);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Rock";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(165, 382);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Paper";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(279, 382);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Scissor";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Enabled = false;
+            this.btnReset.FlatAppearance.BorderSize = 10;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnReset.Location = new System.Drawing.Point(425, 496);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(89, 19);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pbComputerChoice
+            // 
+            this.pbComputerChoice.Location = new System.Drawing.Point(25, 62);
+            this.pbComputerChoice.Name = "pbComputerChoice";
+            this.pbComputerChoice.Size = new System.Drawing.Size(312, 184);
+            this.pbComputerChoice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbComputerChoice.TabIndex = 7;
+            this.pbComputerChoice.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::WindowsFormsApp1.Properties.Resources.scissors;
+            this.pictureBox6.Location = new System.Drawing.Point(255, 269);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(83, 87);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::WindowsFormsApp1.Properties.Resources.paper;
+            this.pictureBox7.Location = new System.Drawing.Point(140, 269);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(83, 87);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 2;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::WindowsFormsApp1.Properties.Resources.rock;
+            this.pictureBox8.Location = new System.Drawing.Point(25, 269);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(83, 87);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 1;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pbPlayerChoice
+            // 
+            this.pbPlayerChoice.Location = new System.Drawing.Point(25, 62);
+            this.pbPlayerChoice.Name = "pbPlayerChoice";
+            this.pbPlayerChoice.Size = new System.Drawing.Size(312, 184);
+            this.pbPlayerChoice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPlayerChoice.TabIndex = 7;
+            this.pbPlayerChoice.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.scissors;
+            this.pictureBox3.Location = new System.Drawing.Point(255, 269);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(83, 87);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.paper;
+            this.pictureBox2.Location = new System.Drawing.Point(140, 269);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 87);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.rock;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 269);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Pristina", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblResult.Location = new System.Drawing.Point(357, 70);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(105, 46);
+            this.lblResult.TabIndex = 10;
+            this.lblResult.Text = "Result:";
+            this.lblResult.Visible = false;
+            // 
+            // frmRockPaperScissorGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(930, 565);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblGameName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmRockPaperScissorGame";
+            this.Text = "Rock Paper Scissor Game";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbComputerChoice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayerChoice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGameName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnScissor;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbPlayerChoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbComputerChoice;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmRockPaperScissorGame : Form
+    {
+        public frmRockPaperScissorGame()
+        {
+            InitializeComponent();
+        }
+
+        enum enChoice {Rock=0,Paper=1,Scissor=2 , NoChoice };
+
+        enum enPlayer { Player=0,Computer=1};
+
+        enum enWinner {Player=0,Computer=1,Draw=2};
+
+        struct stRoundResult
+        {
+            public enChoice PlayerChoice;
+            public enChoice ComputerChoice;
+            public enWinner Winner;
+        };
+
+        enChoice GetComputerChoice()
+        {
+            Random rand = new Random();
+            return (enChoice)rand.Next(0, 3);
+        }
+
+        enChoice ChangeStringChoiceToEnum(string Choice)
+        {
+            switch (Choice)
+            {
+                case "Rock":
+                    return enChoice.Rock;
+                    break;
+                case "Paper":
+                    return enChoice.Paper;
+                    break;
+                case "Scissor":
+                    return enChoice.Scissor;
+                    break;
+            }
+
+            return enChoice.NoChoice ;
+        }
+        void ChangePictureBoxChoice(Button btn)
+        {
+            ChangePictureBoxChoice(ChangeStringChoiceToEnum(btn.Tag.ToString()), enPlayer.Player);
+        }
+
+        void ChangePictureBoxChoice(enChoice Choice ,enPlayer Player )
+        {
+            PictureBox pb = (Player == enPlayer.Player) ? pbPlayerChoice : pbComputerChoice;
+
+            switch (Choice)
+            {
+                case enChoice.Paper:
+                    pb.Image=Resources.Player_Paper;
+                    break;
+                case enChoice.Scissor:
+                    pb.Image=Resources.Player_Scissor;
+                    break;
+                case enChoice.Rock:
+                    pb.Image=Resources.Player_Rock;
+                    break;
+                case enChoice.NoChoice:
+                    pb.Image = null;
+                    break;
+            }
+        }
+
+        enWinner GetWinner(enChoice PlayerChoice , enChoice ComputerChoice)
+        {
+            if(PlayerChoice==ComputerChoice)
+            {
+                return enWinner.Draw;
+            }
+
+            switch(PlayerChoice)
+            {
+                case enChoice.Rock:
+                    if(ComputerChoice == enChoice.Scissor)
+                    {
+                        return enWinner.Player;
+                    }
+                    break;
+                case enChoice.Paper:
+                    if(ComputerChoice==enChoice.Rock)
+                    {
+                        return enWinner.Player;
+                    }
+                    break;
+                case enChoice.Scissor:
+                    if(ComputerChoice==enChoice.Paper)
+                    {
+                        return enWinner.Player;
+                    }
+                    break;
+            }
+
+            return enWinner.Computer;
+        }
+
+         
+
+        stRoundResult PlayRound(enChoice PlayerChoice)
+        {
+            stRoundResult RoundResult = new stRoundResult();
+            RoundResult.PlayerChoice = PlayerChoice;
+            RoundResult.ComputerChoice = GetComputerChoice();
+            RoundResult.Winner = GetWinner(RoundResult.PlayerChoice,RoundResult.ComputerChoice);
+            return RoundResult;
+        }
+
+        
+        void ChangeGameBtnEnabled()
+        {
+            btnRock.Enabled = !btnRock.Enabled;
+            btnPaper.Enabled = !btnPaper.Enabled;
+            btnScissor.Enabled = !btnScissor.Enabled;
+            btnReset.Enabled = !btnReset.Enabled;
+        }
+
+        private void btnGame_Click(object sender, EventArgs e)
+        {
+
+            Button btn = (Button)sender;
+            
+            ChangeGameBtnEnabled();
+            ChangePictureBoxChoice((Button)sender);
+            stRoundResult RoundResult = new stRoundResult();
+            RoundResult = PlayRound(ChangeStringChoiceToEnum(btn.Tag.ToString()));
+            ChangePictureBoxChoice(RoundResult.ComputerChoice,enPlayer.Computer);
+            lblResult.Text = "Winner: "+ RoundResult.Winner.ToString();
+            lblResult.Visible = true;
+
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ChangeGameBtnEnabled();
+            ChangePictureBoxChoice(enChoice.NoChoice,enPlayer.Player);
+            ChangePictureBoxChoice(enChoice.NoChoice, enPlayer.Computer);
+            lblResult.Visible = false;
+            
+        }
+    }
+}
